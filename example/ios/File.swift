52a3c3d1d9db6afcf68d1ccb22f7e6b72293cd71
Switt Kongdachalert <switt1995@gmail.com>
2022-01-03 19:09:22 +0700

    chore: initial commit

---
+++ example/ios/File.swift
@@ -0,0 +1,6 @@
+//
+//  File.swift
+//  NativeVideoExample
+//
+
+import Foundation
---
+++ ios/NativeVideo.h
@@ -0,0 +1,11 @@
+#import <React/RCTBridgeModule.h>
+
+#ifdef __cplusplus
+
+#import "react-native-native-video.h"
+
+#endif
+
+@interface NativeVideo : NSObject <RCTBridgeModule>
+
+@end
